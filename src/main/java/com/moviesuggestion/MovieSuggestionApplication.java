8174cc0cf9a1c64e06d766df031807d05b82d8eb
Justyna Zawalska <justynajzawalska@gmail.com>
2019-03-23 12:41:34 +0100

    Init Spring Boot application

---
+++ src/main/java/com/moviesuggestion/MovieSuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.moviesuggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSuggestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieSuggestionApplication.class, args);
+    }
+
+}
