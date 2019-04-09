f977b552471e577848b50b06954a6f7388a4184d
Justyna Zawalska <justynajzawalska@gmail.com>
2019-04-09 11:04:47 +0200

    Integrate MongoDB

---
+++ src/main/java/com/moviesuggestion/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.moviesuggestion.user;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByFirstName(String firstName);
+
+    List<User> findByLogin(String login);
+
+}
