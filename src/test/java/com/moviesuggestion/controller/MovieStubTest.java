a76fb43026f9b4d7340617952975cb9e598c81d2
Justyna Zawalska <justynajzawalska@gmail.com>
2019-05-21 09:17:45 +0200

    Refactor unit tests

--- src/test/java/com/moviesuggestion/controller/MovieStubTest.java
+++ src/test/java/com/moviesuggestion/controller/MovieStubTest.java
@@ -2,6 +2,7 @@ package com.moviesuggestion.controller;
 
 import com.moviesuggestion.model.Movie;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -9,34 +10,37 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class MovieStubTest {
-
     @Test
-    public void whenCreateMethodIsCalled_thenAMovieIndexIsIncremented() {
-        Movie movie = new Movie(1L, "Title", "Description");
-        int movieAmountBefore = MovieStub.list().size();
-        MovieStub.create(movie);
-        int movieAmountAfter = MovieStub.list().size();
-        assertEquals(movieAmountAfter, movieAmountBefore + 1);
+    @DisplayName("When create method is called then amount of movies increases by one")
+    public void whenCreateMethodIsCalled_thenAmountofMoviesIncreasesByOne() {
+        int movieAmountBeforeInsertingElement = MovieStubMock.list().size();
+        MovieStubMock.create(new Movie(MovieStubMock.getIdIndex() + 1, "Title", "Desc"));
+        int movieAmounfAfterInsertingElement = MovieStubMock.list().size();
+        int diffInMock = movieAmounfAfterInsertingElement - movieAmountBeforeInsertingElement;
+
+        assertEquals(diffInMock, 1);
     }
 
     @Test
+    @DisplayName("When update method is called then the description changes")
     public void whenUpdateMethodIsCalled_thenTheDescriptionChanges() {
-        long lastElementIndex = MovieStub.list().size() - 1;
-        Movie lastAddedMovie = MovieStub.get(lastElementIndex);
-        String descriptionBeforeUpdate = lastAddedMovie.getDescription();
-        String descriptionAfterUpdate = "Updated description";
-        lastAddedMovie.setDescription(descriptionAfterUpdate);
-        MovieStub.update(lastElementIndex, lastAddedMovie);
+        Movie newMovie = MovieStubMock.create(new Movie(MovieStubMock.getIdIndex(), "Title", "Desc"));
+        String descriptionBeforeUpdate = newMovie.getDescription();
+        newMovie.setDescription("Updated description");
+        MovieStubMock.update(newMovie.getId(), newMovie);
+        String descriptionAfterUpdate = newMovie.getDescription();
         assertNotEquals(descriptionBeforeUpdate, descriptionAfterUpdate);
     }
 
     @Test
+    @DisplayName("When delete method is called then amount of movies decreases by one")
     public void whenDeleteMethodIsCalled_thenMovieIsRemoved() {
-        long amountOfMoviesBeforeDeletion = MovieStub.list().size();
-        MovieStub.delete(amountOfMoviesBeforeDeletion - 1); //assuming current number of movies >0
-        int amountOfMoviesAfterDeletion = MovieStub.list().size();
-        assertEquals(amountOfMoviesAfterDeletion, amountOfMoviesBeforeDeletion - 1);
+        Movie newMovie = MovieStubMock.create(new Movie(MovieStubMock.getIdIndex(), "Title", "Desc"));
+        MovieStubMock.setIdIndex(MovieStubMock.getIdIndex() + 1);
+        int amountOfMoviesBeforeDeletionInMock = MovieStubMock.list().size();
+        MovieStubMock.delete(newMovie.getId());
+        int amountOfMoviesAfterDeletionInMock = MovieStubMock.list().size();
+        int diffInMock = amountOfMoviesBeforeDeletionInMock - amountOfMoviesAfterDeletionInMock;
+        assertEquals(diffInMock, 1);
     }
-
-
 }
